87c6301b876757a6f26202a0fdacdaf574596405
dudi <dg.7189477@gmail.com>
2021-04-12 17:42:16 +0300

    dfr

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(document);
+//let test= document.getElementById("test");
+
+
+//let p=document.createElement("p");
+//p.innerHTML="blabla";
+//test.appendChild(p);
+//console.log(test);
+let divbox=document.querySelectorAll('.box');
+console.log(divbox);
+divbox.forEach(el=>el.style.border="1px solid #000000")
